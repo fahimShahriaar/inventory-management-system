301f154471b229d243fd9fa077dc7f752a1c3e3d
fem_97 <fem.nwu@gmail.com>
2022-01-10 17:05:18 +0600

    on going

--- src/components/Admin/Branch/Branch.js
+++ src/components/Admin/Branch/Branch.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Branch = () => {
     return (
-        <div>
-            This is branch
+        <div className="my-6">
+            <h2 className="text-2xl mb-4">Branch List</h2>
+            <ul>
+                <li>Mohakhali</li>
+                <li>Basundhara</li>
+                <li>Mirpur</li>
+            </ul>
+
+            <h2 className="text-2xl my-4">Create Branch</h2>
+            form here... [coming soon...]
         </div>
     );
 };
--- src/components/Admin/Settings/Settings.js
+++ src/components/Admin/Settings/Settings.js
@@ -1,43 +1,62 @@
 import React, { useState } from 'react';
+import Bin from '../Bin/Bin';
+import Branch from '../Branch/Branch';
+import Store from '../Store/Store';
+import User from '../User/User';
 
 const Settings = () => {
     const [displaySetting, setDisplaySetting] = useState({
         branch: true, store: false, bin: false, user: false
     });
-    console.log(displaySetting);
     const handleDisplaySetting = (display) => {
-        const newSetting = { ...displaySetting };
+        let newSetting = { ...displaySetting };
         for (const key in newSetting) {
-            console.log(key, ' ', newSetting[key]);
             newSetting[key] = false;
-            newSetting[display] = true;
-            // setDisplaySetting(newSetting);
         }
+        newSetting[display] = newSetting[display] ? false : true;
+        console.log(newSetting);
+
+        setDisplaySetting(newSetting);
     }
-    handleDisplaySetting("bin");
-    console.log(displaySetting);
 
     return (
-        <div className="flex flex-wrap">
-            <div className="w-6/12 md:w-3/12 px-4">
-                <div className='shadow-md bg-blue-400 text-white p-2'>
-                    Branch
+        <div>
+            <div className="flex flex-wrap">
+                <div className="w-6/12 md:w-3/12 px-4">
+                    <div onClick={() => handleDisplaySetting("branch")} className='shadow-md bg-blue-400 text-white p-2'>
+                        Branch
+                    </div>
+                </div>
+                <div className="w-6/12 md:w-3/12 px-4">
+                    <div onClick={() => handleDisplaySetting("store")} className='shadow-md bg-emerald-500 text-white p-2'>
+                        Store
+                    </div>
+                </div>
+                <div className="w-6/12 md:w-3/12 px-4">
+                    <div onClick={() => handleDisplaySetting("bin")} className='shadow-md bg-yellow-500 text-white p-2'>
+                        Bin
+                    </div>
+                </div>
+                <div className="w-6/12 md:w-3/12 px-4">
+                    <div onClick={() => handleDisplaySetting("user")} className='shadow-md bg-red-400 text-white p-2'>
+                        User
+                    </div>
                 </div>
             </div>
-            <div className="w-6/12 md:w-3/12 px-4">
-                <div className='shadow-md bg-emerald-500 text-white p-2'>
-                    Store
-                </div>
-            </div>
-            <div className="w-6/12 md:w-3/12 px-4">
-                <div className='shadow-md bg-yellow-500 text-white p-2'>
-                    Bin
-                </div>
-            </div>
-            <div className="w-6/12 md:w-3/12 px-4">
-                <div className='shadow-md bg-red-400 text-white p-2'>
-                    User
-                </div>
+
+            <div className="px-4">
+                {
+                    displaySetting.branch && <Branch />
+                }
+                {
+                    displaySetting.store && <Store />
+                }
+                {
+                    displaySetting.bin && <Bin />
+                }
+                {
+                    displaySetting.user && <User />
+                }
             </div>
         </div>
     );
